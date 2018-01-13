1b74324d46d31aa906b62296932131f17debd373
SlawomirR <no.for.spam@wp.pl>
2018-01-13 18:47:58 +0100

    kodilla-testing - 6.4 shape - added tests for ShapeCollector class in ShapeCollectorTestSuite testing class.

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/shape/ShapeCollector.java
@@ -0,0 +1,25 @@
+package com.kodilla.testing.shape;
+
+import java.util.ArrayList;
+
+public class ShapeCollector {
+    private ArrayList<Shape> shapeCollector;
+
+    ShapeCollector() {
+        shapeCollector = new ArrayList<>();
+    }
+
+    void addFigure(Shape shape) {
+    }
+
+    void removeFigure(Shape shape) {
+    }
+
+    Shape getFigure(int index) {
+        return null;
+    }
+
+    String showFigures() {
+        return null;
+    }
+}
