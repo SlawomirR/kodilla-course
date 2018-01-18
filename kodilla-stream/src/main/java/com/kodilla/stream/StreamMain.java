34b14c0f21ef2195f105e08889b29f0231db1fb7
SlawomirR <no.for.spam@wp.pl>
2018-01-18 08:39:50 +0100

    kodilla-stream - 7.1 initial introduction information.

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,37 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to module 7 - Stream");
+
+        SaySomething saySomething = new SaySomething();
+        saySomething.say();
+
+        Processor processor = new Processor();
+        ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
+        processor.execute(executeSaySomething);
+
+        Processor processor1 = new Processor();
+        Executor codeToExecute = () -> System.out.println("This is an example text 3.");
+        processor1.execute(codeToExecute);
+        // or
+        processor1.execute(() -> System.out.println("This is an example text 4."));
+
+        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+
+        System.out.println("Calculating expressions with lambdas");
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a + b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a - b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a * b);
+        expressionExecutor.executeExpression(10, 5, (a, b) -> a / b);
+
+        System.out.println("Calculating expressions with method references");
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::multiplyAByB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::addAToB);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::subBFromA);
+        expressionExecutor.executeExpression(3, 4, FunctionalCalculator::divideAByB);
+    }
+}
