067436a7879c65534957719417ec73703092aa10
SlawomirR <no.for.spam@wp.pl>
2018-01-28 21:33:25 +0100

    kodilla-stream - 8.4 initial introduction information.

---
+++ kodilla-exception/src/main/java/com/kodilla/exception/index/IndexExceptionRunner.java
@@ -0,0 +1,15 @@
+package com.kodilla.exception.index;
+
+import java.util.List;
+
+public class IndexExceptionRunner {
+    public static void main(String[] args) {
+        VideoCollector videoCollector = new VideoCollector();
+        List<String> collection = videoCollector.getCollection();
+
+        String movie = collection.get(0);
+        System.out.println(movie);
+        String movie1 = collection.get(1);
+        System.out.println(movie1);
+    }
+}
