350ead4f794b0e587371fce14cc43fd2a6d052df
SlawomirR <no.for.spam@wp.pl>
2018-01-31 00:58:03 +0100

    kodilla-good-patterns - 9.1 the first working version.

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -1,13 +1,14 @@
 package com.kodilla.good.patterns.challenges;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 class MovieStore {
+    private static final String TITLE_DELIMITER = "!";
+    private static final int POLISH_LANGUAGE = 0;
+    private static final int ENGLISH_LANGUAGE = 1;
 
-    public Map<String, List<String>> getMovies() {
+    private Map<String, List<String>> getMovies() {
 
         List<String> ironManTranslations = new ArrayList<>();
         ironManTranslations.add("Żelazny Człowiek");
@@ -27,5 +28,37 @@ class MovieStore {
         booksTitlesWithTranslations.put("FL", flashTranslations);
 
         return booksTitlesWithTranslations;
+    }
+
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+
+        System.out.println(movieStore.getMovies()
+                .entrySet()
+                .stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(TITLE_DELIMITER))
+        );
+
+        System.out.println(movieStore.getMovies()
+                .entrySet()
+                .stream()
+                .map(Map.Entry::getKey)
+                .collect(Collectors.joining(TITLE_DELIMITER))
+        );
+
+        System.out.println(movieStore.getMovies()
+                .entrySet()
+                .stream()
+                .map(n -> n.getValue().get(POLISH_LANGUAGE))
+                .collect(Collectors.joining(TITLE_DELIMITER))
+        );
+
+        System.out.println(movieStore.getMovies()
+                .entrySet()
+                .stream()
+                .map(n -> n.getValue().get(ENGLISH_LANGUAGE))
+                .collect(Collectors.joining(TITLE_DELIMITER))
+        );
     }
 }
