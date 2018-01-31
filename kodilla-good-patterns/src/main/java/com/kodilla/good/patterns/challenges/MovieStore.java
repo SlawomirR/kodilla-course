8f800f560a04d90db70bf3f35744c8c8742cda14
SlawomirR <no.for.spam@wp.pl>
2018-01-31 01:21:24 +0100

    kodilla-good-patterns - 9.1 the second working version.

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/MovieStore.java
@@ -8,6 +8,29 @@ class MovieStore {
     private static final int POLISH_LANGUAGE = 0;
     private static final int ENGLISH_LANGUAGE = 1;
 
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+
+        System.out.println(movieStore.getMovies()
+                .entrySet()
+                .stream()
+                .map(Object::toString)
+                .collect(Collectors.joining(TITLE_DELIMITER))
+        );
+
+        System.out.println(printAllMovieTitlesInOneLine(ENGLISH_LANGUAGE, TITLE_DELIMITER));
+    }
+
+    private static String printAllMovieTitlesInOneLine(int chooseLanguageNumber, String chooseTitleDelimiter) {
+        MovieStore movieStore = new MovieStore();
+
+        return movieStore.getMovies()
+                .entrySet()
+                .stream()
+                .map(n -> n.getValue().get(chooseLanguageNumber))
+                .collect(Collectors.joining(chooseTitleDelimiter));
+    }
+
     private Map<String, List<String>> getMovies() {
 
         List<String> ironManTranslations = new ArrayList<>();
@@ -28,37 +51,5 @@ class MovieStore {
         booksTitlesWithTranslations.put("FL", flashTranslations);
 
         return booksTitlesWithTranslations;
-    }
-
-    public static void main(String[] args) {
-        MovieStore movieStore = new MovieStore();
-
-        System.out.println(movieStore.getMovies()
-                .entrySet()
-                .stream()
-                .map(Object::toString)
-                .collect(Collectors.joining(TITLE_DELIMITER))
-        );
-
-        System.out.println(movieStore.getMovies()
-                .entrySet()
-                .stream()
-                .map(Map.Entry::getKey)
-                .collect(Collectors.joining(TITLE_DELIMITER))
-        );
-
-        System.out.println(movieStore.getMovies()
-                .entrySet()
-                .stream()
-                .map(n -> n.getValue().get(POLISH_LANGUAGE))
-                .collect(Collectors.joining(TITLE_DELIMITER))
-        );
-
-        System.out.println(movieStore.getMovies()
-                .entrySet()
-                .stream()
-                .map(n -> n.getValue().get(ENGLISH_LANGUAGE))
-                .collect(Collectors.joining(TITLE_DELIMITER))
-        );
     }
 }
