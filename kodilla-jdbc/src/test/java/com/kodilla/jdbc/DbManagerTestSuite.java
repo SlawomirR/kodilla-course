11316cb8c611b14f823b65bd1f482df294b7b603
SlawomirR <no.for.spam@wp.pl>
2018-02-23 15:11:22 +0100

    kodilla-jdbc - 12.6 the first working version on proper database.

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/DbManagerTestSuite.java
@@ -37,4 +37,37 @@ public class DbManagerTestSuite {
         statement.close();
         Assert.assertEquals(5, counter);
     }
+
+    @Test
+    public void testSelectUsersAndPosts() throws SQLException {
+        // Given
+/*
+        Manually added to database to satisfy test:
+        insert into POSTS (USER_ID, BODY) values
+            (1, 'Post nr 3'),
+            (1, 'Post nr 4'),
+            (4, 'Second post'),
+            (4, 'Third post'); commit;
+*/
+        DbManager dbManager = DbManager.getInstance();
+        // When
+        String sqlQuery = "select USERS.FIRSTNAME, USERS.LASTNAME, count(POSTS.BODY) as POSTS_NUMBER\n" +
+                "    from USERS, POSTS where USERS.ID = POSTS.USER_ID\n" +
+                "    group by USERS.LASTNAME, USERS.FIRSTNAME having count(POSTS.BODY) > 2;";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery(sqlQuery);
+        // Then
+        int counter = 0;
+        while (resultSet.next()) {
+            counter++;
+            System.out.println(counter + ". " +
+                    resultSet.getString("FIRSTNAME") + ", " +
+                    resultSet.getString("LASTNAME") + ", " +
+                    resultSet.getInt("POSTS_NUMBER")
+            );
+        }
+        resultSet.close();
+        statement.close();
+        Assert.assertEquals(2, counter);
+    }
 }
