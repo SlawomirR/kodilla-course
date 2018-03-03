eff23e16648434efa4a1be431830e51326a2cdf1
SlawomirR <no.for.spam@wp.pl>
2018-03-03 11:49:28 +0100

    kodilla-hibernate - 13.4 initial introduction information.

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/task/Task.java
@@ -6,6 +6,26 @@ import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
 
+@NamedQueries( {
+        @NamedQuery(
+                name = "Task.retrieveLongTasks",
+                query = "FROM Task WHERE duration > 10"
+        ),
+        @NamedQuery(
+                name = "Task.retrieveShortTasks",
+                query = "FROM Task WHERE duration <= 10"
+        ),
+        @NamedQuery(
+                name = "Task.retrieveTasksWithDurationLongerThan",
+                query = "FROM Task WHERE duration > :DURATION"
+        )
+})
+@NamedNativeQuery(
+        name = "Task.retrieveTasksWithEnoughTime",
+        query = "SELECT * FROM TASKS" +
+                " WHERE DATEDIFF(DATE_ADD(CREATED, INTERVAL DURATION DAY), NOW()) > 5",
+        resultClass = Task.class
+)
 @Entity
 @Table(name = "TASKS")
 public class Task {
@@ -15,6 +35,14 @@ public class Task {
     private int duration;
     private TaskFinancialDetails taskFinancialDetails;
     private TaskList taskList;
+
+    public Task() {}
+
+    public Task(String description, int duration) {
+        this.description = description;
+        this.created = new Date();
+        this.duration = duration;
+    }
 
     @ManyToOne
     @JoinColumn(name = "TASKLIST_ID")
@@ -34,14 +62,6 @@ public class Task {
 
     public void setTaskFinancialDetails(TaskFinancialDetails taskFinancialDetails) {
         this.taskFinancialDetails = taskFinancialDetails;
-    }
-
-    public Task() {}
-
-    public Task(String description, int duration) {
-        this.description = description;
-        this.created = new Date();
-        this.duration = duration;
     }
 
     @Id
