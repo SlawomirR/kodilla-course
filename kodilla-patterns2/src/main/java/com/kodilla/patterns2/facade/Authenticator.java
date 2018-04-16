fa2ab85d59644eefd11747a84bf90c9bdef9635e
SlawomirR <no.for.spam@wp.pl>
2018-04-16 21:40:42 +0200

    kodilla-patterns2 - 20.1 initial introduction information, created sample ugly code for refactoring.

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/Authenticator.java
@@ -0,0 +1,11 @@
+package com.kodilla.patterns2.facade;
+import org.springframework.stereotype.Service;
+import java.util.Random;
+
+@Service
+public class Authenticator {
+    boolean isAuthenticated(Long userId) {
+        Random random = new Random();
+        return random.nextBoolean();
+    }
+}
---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/facade/ProductService.java
@@ -0,0 +1,12 @@
+package com.kodilla.patterns2.facade;
+import org.springframework.stereotype.Service;
+import java.math.BigDecimal;
+import java.util.Random;
+
+@Service
+public class ProductService {
+    BigDecimal getPrice(Long productId) {
+        Random generator = new Random();
+        return new BigDecimal(generator.nextInt(100_000) / 100);
+    }
+}
