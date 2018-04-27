7e5dbe824404a0d51d9bb3704cdf5d3157426b9a
SlawomirR <no.for.spam@wp.pl>
2018-04-27 20:23:32 +0200

    kodilla-patterns2 - 20.5 initial introduction information about Adapter Design Pattern.

---
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/company/newhrsystem/Employee.java
@@ -0,0 +1,56 @@
+package com.kodilla.patterns2.adapter.company.newhrsystem;
+
+import java.math.BigDecimal;
+
+public class Employee {
+    final private String peselId;
+    final private String firstName;
+    final private String lastName;
+    final private BigDecimal baseSalary;
+
+    public Employee(String peselId, String firstName, String lastName, BigDecimal baseSalary) {
+        this.peselId = peselId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.baseSalary = baseSalary;
+    }
+
+    public String getPeselId() {
+        return peselId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public BigDecimal getBaseSalary() {
+        return baseSalary;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Employee)) return false;
+        Employee employee = (Employee) o;
+        return peselId != null ? peselId.equals(employee.peselId) : employee.peselId == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return peselId != null ? peselId.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "peselId='" + peselId + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", baseSalary=" + baseSalary +
+                '}';
+    }
+}
