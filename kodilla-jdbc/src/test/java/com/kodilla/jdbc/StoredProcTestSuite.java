e4ff75786bb6db48c724f88c68a9e0ab13d26981
SlawomirR <no.for.spam@wp.pl>
2018-05-04 15:39:45 +0200

    kodilla-jdbc - 22.3 added test for stored procedure UpdateVipLevels created in MySQL.

---
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -0,0 +1,31 @@
+package com.kodilla.jdbc;
+
+import org.junit.Test;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import static org.junit.Assert.assertEquals;
+
+public class StoredProcTestSuite {
+    @Test
+    public void testUpdateVipLevel() throws SQLException {
+         // Given
+         DbManager dbManager = DbManager.getInstance();
+         String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not set\"";
+         Statement statement = dbManager.getConnection().createStatement();
+         statement.executeUpdate(sqlUpdate);
+         // When
+         String sqlProcedureCall = "CALL UpdateVipLevels()";
+         statement.execute(sqlProcedureCall);
+         // Then
+         String sqlCheckTable = "SELECT COUNT(*) AS HOW_MANY FROM READERS WHERE VIP_LEVEL=\"Not set\"";
+         ResultSet rs = statement.executeQuery(sqlCheckTable);
+         int howMany = -1;
+         if (rs.next()) {
+             howMany = rs.getInt("HOW_MANY");
+         }
+         assertEquals(0, howMany);
+     }
+}
