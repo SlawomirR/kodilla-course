09692f8960b49141dadd62877915711790e52d1d
SlawomirR <no.for.spam@wp.pl>
2018-05-11 09:41:16 +0200

    kodilla-testing2 - 23.2 another try to display dynamic content, created class that find Lenovo laptop on Ebay with price between 279 and 280$ using Selenium.

--- kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
@@ -2,6 +2,7 @@ package com.kodilla.testing2.ebay;
 
 import com.kodilla.testing2.config.WebDriverConfig;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -13,16 +14,21 @@ public class EBayTestingApp {
         WebElement searchField = driver.findElement(By.id("gh-ac"));
         searchField.sendKeys("Laptop");
         searchField.submit();
-        searchField = driver.findElement(By.id("e1-34"));
+/*
+        try {
+            TimeUnit.SECONDS.sleep(30);
+        } catch (InterruptedException e) {
+            //Handle exception
+        }
+*/
+        searchField = driver.findElement(By.id("w3-w0-w1-multiselect[5]"));
         searchField.click();
-        searchField = driver.findElement(By.id("e1-13"));
+        searchField = driver.findElement(By.id("w3-w0-w4-0-0[0]"));
         searchField.sendKeys("279");
-        searchField = driver.findElement(By.id("e1-14"));
+        searchField = driver.findElement(By.id("w3-w0-w4-0-1[0]"));
         searchField.sendKeys("280");
-        searchField.submit();
+        searchField = driver.findElement(By.xpath("//*[@id=\"w3-w0-w4-0\"]/div/button"));
+        searchField.click();
+        driver.findElement(By.xpath("/html/body")).sendKeys(Keys.F5);
     }
 }
-/*
-3. W metodzie main napisz kod, który tworzy Selenium WebDriver (wykorzystaj istniejącą klasę WebDriverConfig do jego
-utworzenia), znajduje pole do wyszukiwania przedmiotów, wstawia do niego napis "Laptop" i zatwierdza wyszukiwanie
-*/
